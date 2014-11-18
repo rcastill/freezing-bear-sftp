9d3267e92771b94c12b1e17647b0917226c0a9c5
Rodolfo Castillo Mateluna <rodolfocastillomateluna@gmail.com>
2014-11-18 18:50:36 +0100

    Main sftp

--- sftp/handler.py
+++ sftp/handler.py
@@ -2,12 +2,13 @@ __author__ = 'Rodolfo'
 
 import paramiko
 from constants import *
+from os import path
 
-class SFTPHandler(paramiko.Transport):
+class SFTPHandler():
     def __init__(self, host, username, password, callback=None):
-        paramiko.Transport.__init__((host, DEFAULT_PORT))
-        self.connect(username=username, password=password)
-        self._sftp = paramiko.SFTPClient.from_transport(self)
+        self._transport = paramiko.Transport((host, DEFAULT_PORT))
+        self._transport.connect(username=username, password=password)
+        self._sftp = paramiko.SFTPClient.from_transport(self._transport)
         self._callback = callback
 
     def set_callback(self, callback):
@@ -15,8 +16,18 @@ class SFTPHandler(paramiko.Transport):
 
     def send(self, rpath, *fpaths):
         for fpath in fpaths:
-            self._sftp(fpath, rpath, callback=self._callback)
+            self._sftp.put(fpath, rpath+path.split(fpath)[-1], callback=self._callback)
 
     def close(self):
         self._sftp.close()
-        self.close()
+        self._transport.close()
+
+if __name__ == '__main__':
+    from sys import argv
+
+    handler = SFTPHandler("ssh2.inf.utfsm.cl", "rcastill", "nw0ikariaM")
+    handler.send("/home/rcastill/", *argv[1:])
+    print "Files sent"
+    handler.close()
+    print "Handler closed."
+    raw_input()
